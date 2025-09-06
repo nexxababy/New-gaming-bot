889f8e661f575f22d3ce353cae04ca847aad1805
Nexxababy <kartik18210@gmail.com>
2025-09-06 11:34:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 API_ID = int(os.environ.get("API_ID", "26344421"))
 API_HASH = os.environ.get("API_HASH", "4bbcf08096c88dfa79c1e8e03af3ab3a")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8474933953:AAHFAqTJGkSiN7ltZUexXV7syiwdZ6YsiPg")
 
 SESSION_NAME = "gaming-bot"
 DB_FILE = "gaming_bot_db.json"
