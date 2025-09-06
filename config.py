0d0d695f8e3cfae0a459e1f4709cffbd095b3939
Nexxababy <kartik18210@gmail.com>
2025-09-06 13:48:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,4 @@
-import os
-
-API_ID = int(os.environ.get("API_ID", "26344421"))
-API_HASH = os.environ.get("API_HASH", "4bbcf08096c88dfa79c1e8e03af3ab3a")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8474933953:AAHFAqTJGkSiN7ltZUexXV7syiwdZ6YsiPg")
-
-SESSION_NAME = "gaming-bot"
-DB_FILE = "gaming_bot_db.json"
-
-START_COINS = 100
-DAILY_REWARD = 50
-DAILY_COOLDOWN_HOURS = 20
+API_ID = 123456
+API_HASH = "your_api_hash"
+BOT_TOKEN = "123456:ABC-DEF"
+DATA_DIR = "storage"
