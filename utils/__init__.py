5d943673c965fda539720a97fe825f15b0add7f4
Nexxababy <kartik18210@gmail.com>
2025-09-06 13:54:28 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,9 @@
-# empty
+import pkgutil
+import importlib
+
+__all__ = []
+for _loader, modname, _ispkg in pkgutil.iter_modules(__path__):
+    module = importlib.import_module(f"{__name__}.{modname}")
+    globals()[modname] = module
+    __all__.append(modname)
+
