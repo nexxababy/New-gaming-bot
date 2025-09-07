e08d2bcf4dabdfb3e49e807c28588a1940ac6806
Nexxababy <kartik18210@gmail.com>
2025-09-07 10:58:55 +0530

    Delete utils/helpers.py

--- utils/helpers.py
+++
@@ -1,9 +0,0 @@
-def pretty_name(user):
-    """Convert user object or string into a pretty display name"""
-    if isinstance(user, str):
-        return user.title()
-    elif hasattr(user, "first_name"):
-        return f"{user.first_name} {user.last_name or ''}".strip()
-    elif hasattr(user, "username"):
-        return f"@{user.username}"
-    return str(user)
